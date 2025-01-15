f4f8a44262bd0306062ea3f8323f70a32f02b15f
Seasoil <zlwthinkfuhua666@outlook>
2025-01-15 21:44:16 +0800

    Site updated: 2025-01-15 21:44:16

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/14/hello-world/","2025/01/15/新领域指南/博客以及对应操作/","2024/12/14/note/11.30-12.7note/"];function toRandomPost(){
+var posts=["2024/12/14/hello-world/","2024/12/14/note/11.30-12.7note/","2025/01/15/新领域指南/博客以及对应操作/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
